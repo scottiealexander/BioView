47e63467cf0b72d7ed2c08bcf13c23f70b1b4770
scottiealexander <scottiealexander11@gmail.com>
2016-07-13 17:04:25 -0700

    Enable caching of loaded file to speed ui loading

--- GetImagePath.m
+++ GetImagePath.m
@@ -1,7 +1,8 @@
 function pth = GetImagePath(typ)
 % GetImagePath
 %
-% Description: prompt user for image path
+% Description: prompt user for image path, uses the last image loaded (from the
+%              .last_image.tmp file) if it exists
 %
 % Syntax: pth = GetImagePath(type)
 %
@@ -11,16 +12,62 @@ function pth = GetImagePath(typ)
 % Out:
 %       pth - the users selected file path (empty if canceled)
 %
-% Updated: 2016-07-12
+% Updated: 2016-07-13
 % Scottie Alexander
 %
 % Please report bugs to: scottiealexander11@gmail.com
 
-[fname,fdir] = uigetfile(typ,'Please select an image/data file');
-if isequal(fname,0) || isequal(fdir,0)
-    pth = '';
+cur_dir = fileparts(mfilename('fullpath'));
+tmp_file = fullfile(cur_dir, '.last_image.tmp');
+
+def_file = '';
+base_dir = pwd();
+pth = '';
+
+if exist(tmp_file,'file')
+    fid = fopen(tmp_file,'r');
+    try
+        def_file = strtrim(transpose(fread(fid,'*char')));
+    catch e
+    end
+    fclose(fid);
+
+    if exist(def_file,'file') == 0
+        def_file = '';
+        def_dir = base_dir;
+    else
+        [def_dir,fname,ext] = fileparts(def_file);
+        def_file = [fname ext];
+    end
 else
-    pth = fullfile(fdir,fname);
+    def_dir = base_dir;
 end
+
+try
+    cd(def_dir);
+
+    [fname,fdir] = uigetfile(typ,'Please select an image/data file',def_file);
+
+    cd(base_dir);
+
+    if isequal(fname,0) || isequal(fdir,0)
+        pth = '';
+    else
+        pth = fullfile(fdir,fname);
+
+        fid = fopen(tmp_file,'w');
+        try
+            fprintf(fid,'%s\n',pth);
+        catch e
+        end
+
+        fclose(fid);
+    end
+catch e
+    cd(base_dir);
+    rethrow(e);
+end
+
+cd(base_dir);
 
 end
